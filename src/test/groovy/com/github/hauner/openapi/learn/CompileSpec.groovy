0b4d349b7374dc63088540f4a361a4f00f7d89c5
Martin Hauner <martin.hauner@gmx.net>
2019-07-14 11:09:27 +0200

    compile class with java compiler

--- src/test/groovy/com/github/hauner/openapi/learn/CompileSpec.groovy
+++ src/test/groovy/com/github/hauner/openapi/learn/CompileSpec.groovy
@@ -1,10 +1,17 @@
 package com.github.hauner.openapi.learn
 
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
+
+import javax.tools.ToolProvider
+import javax.tools.JavaCompiler
 
 class CompileSpec extends Specification {
 
-    void "generated class compiles" () {
+    @Rule TemporaryFolder target
+
+    void "generated class compiles with groovy compiler" () {
         def loader = new GroovyClassLoader()
         String sourceCode = "interface Api {}"
 
@@ -13,5 +20,38 @@ class CompileSpec extends Specification {
 
         then:
         clazz
+    }
+
+    void "generated class compiles with java compiler" () {
+        def pkg = 'com.github.hauner.openapi.test'
+        def name = 'Test'
+
+        File source = saveSource (pkg, name, """\
+package $pkg;
+
+interface $name {
+}
+ """)
+
+        when:
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler()
+        compiler.run(null, null, null, source.getPath())
+
+        then:
+        def loader = new URLClassLoader([target.root.toURI ().toURL ()] as URL[])
+        Class.forName("$pkg.$name", true, loader)
+    }
+
+    private File saveSource (String pkg, String name, String sourceCode) {
+        def targetFolder = target.root.absolutePath
+
+        def sourceFolder = (
+            [targetFolder] + (pkg.split (/\./) as List<String>) + ["${name}.java"]
+        ).join (File.separator)
+
+        File source = new File (sourceFolder)
+        source.parentFile.mkdirs ()
+        source.write (sourceCode, 'UTF-8')
+        source
     }
 }
