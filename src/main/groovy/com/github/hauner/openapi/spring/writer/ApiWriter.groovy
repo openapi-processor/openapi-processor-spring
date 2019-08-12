c6c42ad544370a0c5fc67c05172a55f881e48a32
Martin Hauner <martin.hauner@gmx.net>
2019-08-12 17:32:02 +0200

    fixed project name

--- src/main/groovy/com/github/hauner/openapi/spring/writer/ApiWriter.groovy
+++ src/main/groovy/com/github/hauner/openapi/spring/writer/ApiWriter.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 https://github.com/hauner/openapi-spring-generator
+ * Copyright 2019 https://github.com/hauner/openapi-spring-generatr
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
