88f469102df7b3fc4a4017e875e68f6d24903ec4
Martin Hauner <martin.hauner@gmx.net>
2019-08-15 12:42:32 +0200

    data type format handling

--- src/testInt/groovy/com/github/hauner/openapi/generatr/GeneratrTest.groovy
+++ src/testInt/groovy/com/github/hauner/openapi/generatr/GeneratrTest.groovy
@@ -44,6 +44,7 @@ class GeneratrTest {
     @Parameters(name = "{0}")
     static Collection<String> sources () {
         return [
+            'response-data-types',
             'no-response-content'
         ]
     }
