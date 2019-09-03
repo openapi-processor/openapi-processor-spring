f661f0b37710a0e56768ad2a2373445fa85cae1f
Martin Hauner <martin.hauner@gmx.net>
2019-09-03 08:48:22 +0200

    easier integration tests

---
+++ src/testInt/groovy/com/github/hauner/openapi/generatr/GeneratrEndToEndTest.groovy
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 the original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.hauner.openapi.generatr
+
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+
+/**
+ * using Junit so IDEA adds a "<Click to see difference>" when using assertEquals().
+ */
+
+@RunWith(Parameterized)
+class GeneratrEndToEndTest extends GeneratrTestBase {
+
+    @Parameters(name = "{0}")
+    static Collection<String> sources () {
+        return [
+            'no-response-content',
+            'response-simple-data-types',
+            'response-complex-data-types'
+        ]
+    }
+
+    GeneratrEndToEndTest (String source) {
+        super(source)
+    }
+
+}
--- src/testInt/groovy/com/github/hauner/openapi/generatr/GeneratrTestBase.groovy
+++ src/testInt/groovy/com/github/hauner/openapi/generatr/GeneratrTestBase.groovy
@@ -1,19 +1,3 @@
-/*
- * Copyright 2019 the original authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.github.hauner.openapi.generatr
 
 import com.github.difflib.DiffUtils
@@ -21,39 +5,21 @@ import com.github.difflib.UnifiedDiffUtils
 import com.github.hauner.openapi.spring.generatr.ApiOptions
 import com.github.hauner.openapi.spring.generatr.SpringGeneratr
 import groovy.io.FileType
-import org.junit.Rule;
+import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.TemporaryFolder
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.rules.TemporaryFolder;
+
 
 import static org.junit.Assert.assertEquals;
 
-
-/**
- * using Junit so IDEA adds a "<Click to see difference>" when using assertEquals().
- */
-
-@RunWith(Parameterized)
-class GeneratrTest {
+public class GeneratrTestBase {
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder()
 
-    @Parameters(name = "{0}")
-    static Collection<String> sources () {
-        return [
-            'no-response-content',
-            'response-simple-data-types',
-            'response-complex-data-types',
-            //'ref-into-another-file' // pending
-        ]
-    }
-
     String source
 
-    GeneratrTest(String source) {
+    GeneratrTestBase(String source) {
         this.source = source
     }
 
