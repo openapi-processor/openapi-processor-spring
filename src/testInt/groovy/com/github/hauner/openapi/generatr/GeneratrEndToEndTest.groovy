fb2b5807eb6b584b6c88c45f90186fc31b1cbc36
Martin Hauner <martin.hauner@gmx.net>
2019-12-04 23:18:19 +0100

    no longer pending

--- src/testInt/groovy/com/github/hauner/openapi/generatr/GeneratrEndToEndTest.groovy
+++ src/testInt/groovy/com/github/hauner/openapi/generatr/GeneratrEndToEndTest.groovy
@@ -39,7 +39,8 @@ class GeneratrEndToEndTest extends GeneratrTestBase {
           //  new TestSet(name: 'params-complex-data-types'),
             new TestSet(name: 'params-path-simple-data-types'),
             new TestSet(name: 'params-request-body'),
-            new TestSet(name: 'params-spring-pageable-mapping')
+            new TestSet(name: 'params-spring-pageable-mapping'),
+            new TestSet(name: 'params-enum')
         ]
     }
 
