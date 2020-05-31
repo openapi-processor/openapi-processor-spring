c1311b7a6175464f3da25fc09133f1460fe2a1d1
Martin Hauner <martin.hauner@gmx.net>
2020-05-31 19:22:52 +0200

    cleanup

--- src/test/groovy/com/github/hauner/openapi/core/writer/MethodWriterSpec.groovy
+++ src/test/groovy/com/github/hauner/openapi/core/writer/MethodWriterSpec.groovy
@@ -19,7 +19,6 @@ package com.github.hauner.openapi.core.writer
 import com.github.hauner.openapi.core.model.parameters.ParameterBase
 import com.github.hauner.openapi.core.support.TestMappingAnnotationWriter
 import com.github.hauner.openapi.core.support.TestParameterAnnotationWriter
-import com.github.hauner.openapi.core.model.parameters.QueryParameter
 import com.github.hauner.openapi.spring.converter.ApiOptions
 import com.github.hauner.openapi.spring.model.Endpoint
 import com.github.hauner.openapi.spring.model.HttpMethod
