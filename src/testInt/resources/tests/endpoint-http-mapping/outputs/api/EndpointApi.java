cd232b7caf2f2bbee487b5039d5d80c633d9b160
Martin Hauner <martin.hauner@gmx.net>
2023-09-17 09:27:52 +0200

    #192, test all mappings

--- src/testInt/resources/tests/endpoint-http-mapping/outputs/api/EndpointApi.java
+++ src/testInt/resources/tests/endpoint-http-mapping/outputs/api/EndpointApi.java
@@ -1,24 +1,39 @@
 package generated.api;
 
 import generated.support.Generated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Generated(value = "openapi-processor-spring", version = "test")
 public interface EndpointApi {
 
+    @DeleteMapping(path = "/endpoint")
+    void deleteEndpoint();
+
     @GetMapping(path = "/endpoint")
     void getEndpoint();
 
-    @PutMapping(path = "/endpoint")
-    void putEndpoint();
+    @RequestMapping(path = "/endpoint", method = RequestMethod.HEAD)
+    void headEndpoint();
+
+    @RequestMapping(path = "/endpoint", method = RequestMethod.OPTIONS)
+    void optionsEndpoint();
+
+    @PatchMapping(path = "/endpoint")
+    void patchEndpoint();
 
     @PostMapping(path = "/endpoint")
     void postEndpoint();
 
-    @PatchMapping(path = "/endpoint")
-    void patchEndpoint();
+    @PutMapping(path = "/endpoint")
+    void putEndpoint();
+
+    @RequestMapping(path = "/endpoint", method = RequestMethod.TRACE)
+    void traceEndpoint();
 
 }
