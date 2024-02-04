f7308405827882d2f0dabd825bafb0e4b58fd526
Martin Goldhahn <mgoldhahn@gmail.com>
2024-02-04 18:11:01 +0100

    add test cases in pending test suite for almost all combinations of parser, and api, had to filter out swagger and OpenAPI 3.1 since they generate classes with different names

--- src/testInt/kotlin/io/openapiprocessor/spring/ProcessorPendingSpec.kt
+++ src/testInt/kotlin/io/openapiprocessor/spring/ProcessorPendingSpec.kt
@@ -8,7 +8,7 @@ package io.openapiprocessor.spring
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.engine.spec.tempdir
 import io.kotest.matchers.booleans.shouldBeTrue
-import io.openapiprocessor.core.parser.ParserType.INTERNAL
+import io.openapiprocessor.core.parser.ParserType.*
 import io.openapiprocessor.test.*
 import io.openapiprocessor.test.API_30
 import io.openapiprocessor.test.TestSet
@@ -34,8 +34,16 @@ class ProcessorPendingSpec: StringSpec({
 })
 
 private fun sources(): Collection<TestSet> {
-    return listOf(
-        testSet("reactive-response-types", INTERNAL, API_30, model = "default", outputs = "outputs.yaml", expected = "outputs"),
-//        testSet("reactive-response-types", INTERNAL, API_31, model = "default", outputs = "outputs.yaml", expected = "outputs"),
-    )
+    return listOf("reactive-response-types", "reactive-response-types2", "reactive-response-types3")
+        .flatMap { testSet ->
+            listOf(INTERNAL, OPENAPI4J, SWAGGER)
+                .flatMap { parser ->
+                    listOf(API_30, API_31)
+                        .map { api ->
+                            testSet(testSet, parser, api, model = "default", outputs = "outputs.yaml", expected = "outputs")
+                        }
+                }
+        }
+            .filter { it.parser != "swagger" && it.openapi != API_31 } // swagger parser in OpenAPI 3.1 generates classes with different names.
+
 }
