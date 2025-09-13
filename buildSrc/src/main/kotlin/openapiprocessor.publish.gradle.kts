4ef602c951b9f80674816a54687bfe6acd5b18c5
Martin Hauner <martin.hauner@gmx.net>
2025-09-13 22:39:29 +0200

    update publishing

--- buildSrc/src/main/kotlin/openapiprocessor.publish.gradle.kts
+++ buildSrc/src/main/kotlin/openapiprocessor.publish.gradle.kts
@@ -1,44 +1,18 @@
-import io.openapiprocessor.build.core.dsl.initFrom
-import io.openapiprocessor.build.core.dsl.initSignKey
-import io.openapiprocessor.build.core.dsl.sonatype
-import io.openapiprocessor.build.core.getPomProperties
-import org.gradle.accessors.dm.LibrariesForLibs
-
 plugins {
     `maven-publish`
     signing
-   id("io.openapiprocessor.build.plugin.publish-base")
+    id("io.openapiprocessor.build.plugin.publish")
 }
 
-// central plugin setup must run in the context of the applying project
-plugins.apply("io.openapiprocessor.build.plugin.publish-central")
-
-// see buildSrc/build.gradle.kts
-val libs = the<LibrariesForLibs>()
-
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("openapiprocessor") {
-                from(components["java"])
-
-                pom {
-                    pom.initFrom(getPomProperties(project))
-                }
-            }
+publishing {
+    publications {
+        create<MavenPublication>("openapiprocessor") {
+            from(components["java"])
         }
-
-        repositories {
-            sonatype(project)
-        }
-    }
-
-    signing {
-        initSignKey()
-        sign(publishing.publications["openapiprocessor"])
     }
 }
 
-publishProcessor {
+publishingCentral {
     deploymentName = "spring"
+    waitFor = "VALIDATED"
 }
